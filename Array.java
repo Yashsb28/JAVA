f35650be35ade755dfdb873168186acc42cf48e0
Yashsb28 <78728820+Yashsb28@users.noreply.github.com>
2021-08-29 14:42:51 +0530

    Update and rename Array to Array.java

--- Array.java
+++ Array.java
@@ -6,28 +6,28 @@ public class matrixAdditionAndMultiplication {
 			int a[][]={{1,3,4},{2,4,3},{3,4,5}};    
 			int b[][]={{1,3,4},{2,4,3},{1,2,4}};    
 			      
-			int y[][]=new int[3][3];  
+			int c[][]=new int[3][3];  
 			
 			//matrix addition  
 			System.out.println("matrix addition:");
 			for(int i=0;i<3;i++){    
 			for(int j=0;j<3;j++){ 
 				
-			y[i][j]=a[i][j]+b[i][j];
+			c[i][j]=a[i][j]+b[i][j];
 			
-			System.out.print(y[i][j]+" ");    
+			System.out.print(c[i][j]+" ");    
 			}    
 			System.out.println(); 
 			}
 			System.out.println("matrix multiplication:");
-	     //matrix multiplication
+	     		//matrix multiplication
 			 for(int i=0;i<3;i++){    
 		     for(int j=0;j<3;j++){ 
-		    	 y[i][j]=0; 
+		    	 c[i][j]=0; 
 		     for(int k=0;k<3;k++) {
-			y[i][j]+=a[i][k]*b[k][j];
+			c[i][j]+=a[i][k]*b[k][j];
 				}
-			System.out.print(y[i][j]+" ");    
+			System.out.print(c[i][j]+" ");    
 			}    
 			System.out.println(); 
 			}    
